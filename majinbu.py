a74ad017548670ac9677022aede7ad5e1a32e3c5
Abdul Rahman <abdulrahman8686475239@gmail.com>
2026-08-11 20:25:40 +0530

    newboy

--- majinbu.py
+++ majinbu.py
@@ -1,11 +1,12 @@
 import os
+
 from dotenv import load_dotenv
 
 load_dotenv()
 
 # Read from environment
-api_key = os.environ.get('API_KEY')
-database = os.environ.get('DATABASE_URL', 'default.db')
+api_key = os.environ.get("API_KEY")
+database = os.environ.get("DATABASE_URL", "default.db")
 
 print(f"Using API key: {api_key}")
 print(f"Using database: {database}")
